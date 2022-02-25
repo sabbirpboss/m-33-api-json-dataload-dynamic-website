06d7ecb512af9e562814339ba67f5f2931c11b0e
sabbirpboss <sabbir.ah799@gmail.com>
2022-02-25 15:58:01 +0600

    post updated

--- api-intro/posts.js
+++ api-intro/posts.js
@@ -2,11 +2,18 @@
 function loadPosts() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((posts) => displayPosts(posts));
+      .then((data) => displayPosts(data));
   }
-  
+  loadPosts();
   function displayPosts(posts) {
+      const postContainer = document.getElementById('posts');
     for (const post of posts){
-      console.log(post.title);
+        const div = document.createElement('div');
+        div.classList.add('post'); //adding a new class name
+        div.innerHTML = `
+        <h3> ${post.id}- New title: ${post.title}</h3>
+        <p>New description: ${post.body}</p>
+        `;
+        postContainer.appendChild(div);
     }
   }
