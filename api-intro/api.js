5576fb9980486f1bcd0a7d1ab6eb92020dcf70d8
sabbirpboss <sabbir.ah799@gmail.com>
2022-02-25 14:36:52 +0600

    api file updated

--- api-intro/api.js
+++ api-intro/api.js
@@ -3,3 +3,15 @@ function loadData() {
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
+function loadData() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+// loadData();
+function displayUsers(json) {
+  // const ul = document.getElementById('users');
+  for(const user of json){
+    console.log(user.name);
+  } 
+}
--- api-intro/json.js
+++ api-intro/json.js
@@ -1,5 +1,5 @@
 //JSON-JavaScript Object Notation
-const user = {id: 11, name: 'Gorib Amir', job: 'google'}
+const user = { id: 11, name: "Gorib Amir", job: "google" };
 
 const stringified = JSON.stringify(user);
 
@@ -7,16 +7,16 @@ const stringified = JSON.stringify(user);
 // console.log(stringified);
 
 const shop = {
-    name: 'Bablu store',
-    address: 'New Road',
-    profit: 15000,
-    products: ['laptop', 'mobile', 'pepsi'],
-    owner: {
-        name: 'Bablur Baap',
-        profession: 'dokandar'
-    },
-    isExpensive: false
-}
+  name: "Bablu store",
+  address: "New Road",
+  profit: 15000,
+  products: ["laptop", "mobile", "pepsi"],
+  owner: {
+    name: "Bablur Baap",
+    profession: "dokandar",
+  },
+  isExpensive: false,
+};
 const shopStringified = JSON.stringify(shop);
 console.log(shop);
 console.log(shopStringified);
